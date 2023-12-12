860c4ffaa46458078626fc96f66f71f7884bec08
six <104098327+eerz@users.noreply.github.com>
2023-12-12 19:13:59 +0530

    commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World")
+	})
+
+	app.Get("/env", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, ENV!" + os.Getenv("TEST_ENV"))
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(aperror {
+		return c.SendString("Hello, ENV!" + os.Getenv("TEST_ENV"))
+	})p.Listen("0.0.0.0:" + port))
+}
